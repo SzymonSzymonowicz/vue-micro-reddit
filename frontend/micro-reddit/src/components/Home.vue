0a91f920fd78715ea37f26b56c88f49892c62b27
Szymon Szymonowicz <sz.szymonowicz5@gmail.com>
2021-06-20 18:19:46 +0200

    FE Separeted Post component

--- frontend/micro-reddit/src/components/Home.vue
+++ frontend/micro-reddit/src/components/Home.vue
@@ -3,38 +3,17 @@
     <button @click="sortById" class="btn btn-primary mt-3">
       Sortuj po NEW
     </button>
-    <div v-for="post in posts" :key="post.id" class="post">
-      <div class="header">
-        <div class="title">{{ post.title }} # {{ post.id }}</div>
-        <div class="auth">Autor: {{ post.author }}</div>
-      </div>
-      <div class="details">
-        <div class="subreddit">
-          Subreddit:
-          <router-link :to="{ path: `/r/${post.subreddit}` }">{{
-            post.subreddit
-          }}</router-link>
-        </div>
-        <div class="date">
-          {{ post.creationDate }}
-        </div>
-      </div>
-      <div class="image" v-if="post.imagePath">
-        <img :src="post.imagePath" />
-      </div>
-      <div class="content">{{ post.content }}</div>
-      <div class="video" v-if="post.videoUrl">
-        <iframe width="640" height="360" :src="post.videoUrl" />
-      </div>
-    </div>
+    <Post v-for="post in posts" :key="post.id" :post="post" />
   </div>
 </template>
 
 <script>
 import { getMyPosts } from "../service/post";
+import Post from "@/components/Post";
 
 export default {
   name: "Home",
+  components: { Post },
   data() {
     return {
       posts: [],
@@ -71,43 +50,11 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .home {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  .post {
-    width: 80%;
-    min-width: 200px;
-    margin: 20px 0px;
-    padding: 20px;
-    border: black 2px solid;
-    background: lightgray;
-    border-radius: 10px;
-    .title {
-      font-size: 36px;
-    }
-    .header {
-      display: flex;
-      flex-direction: column;
-    }
-    .content {
-      margin: 20px 0px;
-    }
-    .image {
-      display: flex;
-      justify-content: center;
-      img {
-        width: 80%;
-        height: 80%;
-      }
-    }
-    .video {
-      display: flex;
-      justify-content: center;
-    }
-  }
 }
 </style>
---
+++ frontend/micro-reddit/src/components/Post.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="post">
+    <div class="header">
+      <div class="title">{{ post.title }} # {{ post.id }}</div>
+      <div class="auth">Autor: {{ post.author }}</div>
+    </div>
+    <div class="details">
+      <div class="subreddit">
+        Subreddit:
+        <router-link :to="{ path: `/r/${post.subreddit}` }"
+          >{{ post.subreddit }}
+        </router-link>
+      </div>
+      <div class="date">
+        {{ post.creationDate }}
+      </div>
+    </div>
+    <div class="image" v-if="post.imagePath">
+      <img :src="post.imagePath" />
+    </div>
+    <div class="content">{{ post.content }}</div>
+    <div class="video" v-if="post.videoUrl">
+      <iframe width="640" height="360" :src="post.videoUrl" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Post",
+  props: {
+    post: {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.post {
+  width: 80%;
+  min-width: 200px;
+  margin: 20px 0px;
+  padding: 20px;
+  border: black 2px solid;
+  background: lightgray;
+  border-radius: 10px;
+  .title {
+    font-size: 36px;
+  }
+  .header {
+    display: flex;
+    flex-direction: column;
+  }
+  .content {
+    margin: 20px 0px;
+  }
+  .image {
+    display: flex;
+    justify-content: center;
+    img {
+      width: 50%;
+      height: 50%;
+    }
+  }
+  .video {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
